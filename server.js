549a5ce7610e5ac3c131afd6c9c3d3def4fb8049
Pixeladed <vietminhle98@gmail.com>
2015-04-13 18:15:56 +0800

    make server heroku compatible

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var path = require("path");
+var fs = require("fs");
 var app = express();
+var port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '/Dash_app')));
 
-app.listen(3000);
+app.listen(port);
