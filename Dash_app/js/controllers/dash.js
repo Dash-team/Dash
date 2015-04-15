6b7aa9d56a919d9defbd965eea4e8c2d6d8320b3
Pixeladed <vietminhle98@gmail.com>
2015-04-15 18:35:55 +0800

    revamp the dashboard

--- Dash_app/js/controllers/dash.js
+++ Dash_app/js/controllers/dash.js
@@ -1,4 +1,6 @@
 app.controller("dashController",["$scope","$rootScope", function($scope, $rootScope) {
+	$scope.user = $rootScope.user;
+	$scope.status = "Economics is coming up in 23 minutes";
 	$scope.subject = "Economics";
 	$scope.timeleft = "32 minutes";
 	$scope.noteCount = 17;
@@ -13,6 +15,9 @@ app.controller("dashController",["$scope","$rootScope", function($scope, $rootSc
 			console.log($rootScope.reminders);
 			$scope.reValue = "";
 		}
+	}
+	$scope.colorOf = function(index) {
+		return $scope.user.subjects[index].color;
 	}
 	$scope.assessments = $rootScope.user.assessments;
 }]);
--- Dash_app/js/controllers/main.js
+++ Dash_app/js/controllers/main.js
@@ -3,8 +3,11 @@ var app = angular.module("app",["ngRoute"]);
 app.filter('unsafe', function($sce) { return $sce.trustAsHtml; });
 
 app.run(['$rootScope', function($rootScope){
-	$rootScope.user = {};
-	$rootScope.user.assessments = [
+	$rootScope.user = {
+		"fullName": "Kevin Reyes",
+		"password": "kevin",
+		"reminders": ["computer science investigation", "ask mom for food", "revise for math test", "have dinner"],
+		"assessments": [
 			{
 				"name": "Math investigation",
 				"progress": 40,
@@ -15,7 +18,26 @@ app.run(['$rootScope', function($rootScope){
 				"progress": 72,
 				"dueDate": "19/3/2015"
 			}
-		];
+		],
+		"subjects": [
+			{
+				"name":"Math",
+				"color": "#5394ff"
+			},
+			{
+				"name":"Physics",
+				"color":"#F2676F"
+			},
+			{
+				"name":"Economics",
+				"color":"#68E59F"
+			},
+			{
+				"name":"English",
+				"color":"#FCE964"
+			}
+		]
+	}
 }]);
 
 app.config(function($routeProvider) {
