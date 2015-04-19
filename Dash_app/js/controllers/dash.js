f97bb784ccf36dc5b6f7155e2fa069e09ea4018c
Pixeladed <vietminhle98@gmail.com>
2015-04-19 13:38:39 +0800

    remove old dash.js

--- Dash_app/js/controllers/dash.js
+++ Dash_app/js/controllers/dash.js
@@ -1,21 +1,7 @@
 app.controller("dashController",["$scope","$rootScope", function($scope, $rootScope) {
 	$scope.user = $rootScope.user;
 	$scope.status = "Economics is coming up in 23 minutes";
-	$scope.subject = "Economics";
-	$scope.timeleft = "32 minutes";
-	$scope.noteCount = 17;
-	$scope.resourceCount = 63;
 	// reminder
-	$scope.reValue = "";
-	$scope.handle = function(event) {
-		if (event.keyCode == 13) {
-			event.preventDefault();
-			alertify.success("added reminder: \"" + $scope.reValue + "\"");
-			$rootScope.user.reminders.push($scope.reValue);
-			console.log($rootScope.reminders);
-			$scope.reValue = "";
-		}
-	}
 	$scope.colorOf = function(index) {
 		return $scope.user.subjects[index].color;
 	}
