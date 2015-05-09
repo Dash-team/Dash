5baa662b7775c78553133a8caa2c26730ac9cdd2
Pixeladed <vietminhle98@gmail.com>
2015-05-09 19:31:23 +0800

    Redesign flash

--- Dash_app/js/controllers/dash.js
+++ Dash_app/js/controllers/dash.js
@@ -1,6 +1,18 @@
 app.controller("dashController",["$scope","$rootScope", function($scope, $rootScope) {
 	$scope.user = $rootScope.user;
 	$scope.status = "Economics is coming up in 23 minutes";
+	$scope.flash = {
+		now: {
+			title: "Biology",
+			des: "Classification research",
+			icon: "assets/assignment.svg"
+		},
+		next: {
+			title: "Reminder",
+			des: "Submit Task 7",
+			icon: "assets/alarm.svg"
+		}
+	}
 	// reminder
 	$scope.colorOf = function(index) {
 		return $scope.user.subjects[index].color || "#288dff";
