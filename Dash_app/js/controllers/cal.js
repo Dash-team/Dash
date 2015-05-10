9e4500a0cba3e96144cc9b63400a9b415884bcad
Pixeladed <vietminhle98@gmail.com>
2015-05-10 05:56:16 +0000

    display calendar

--- Dash_app/js/controllers/cal.js
+++ Dash_app/js/controllers/cal.js
@@ -1,3 +1,8 @@
 app.controller("calendarController",["$scope", function($rootscope, $scope) {
-
+    
+    $(document).ready(function() {
+        $("#mini-clndr").clndr({
+            template: $("#calendar-template").html()
+        });
+    });  
 }]);
